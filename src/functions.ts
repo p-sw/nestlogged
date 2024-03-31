90871c1ecc75fa0136ad92ced4b0ff401dcd2a35
p-sw <shinwoo.park@psw.kr>
2024-03-31 16:29:20 +0900

    feat: add improved beta function

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,63 @@
 export const notIncludedSymbol = Symbol("notIncluded");
 
+export function includeObjectSync(
+  ocv: any,
+  opt: {
+    paths: string[],
+  }
+) {
+  let current = Array.isArray(ocv) ? [] : typeof ocv === 'object' ? {} : ocv
+  opt.paths.forEach((dotpath) => {
+    let query = ocv;
+    let objRef = current;
+    const path = dotpath.split('.');
+    for (const [index, key] of Object.entries(path)) {
+      query = query[key]
+      if (query !== undefined && objRef[key] === undefined) {
+        if (typeof query === 'object') {
+          if (Array.isArray(query)) {
+            objRef[key] = []
+          } else {
+            objRef[key] = {}
+          }
+        }
+      }
+      if (typeof query !== 'object' || index === (path.length - 1).toString()) {
+        objRef[key] = query;
+        break
+      }
+      objRef = objRef[key]
+    }
+  })
+  return current;
+}
+
+export function excludeObjectSync(
+  ocv: any,
+  opt: {
+    paths: string[]
+  }
+) {
+  const copied = typeof ocv === 'object' ? Array.isArray(ocv) ? [...ocv] : { ...ocv } : ocv;
+  opt.paths.forEach((dotpath) => {
+    let objRef = copied;
+    const path = dotpath.split('.')
+    const lastIndex = (path.length - 1).toString()
+    for (const [index, key] of Object.entries(path)) {
+      if (index === lastIndex) {
+        delete objRef[key];
+        break;
+      }
+      objRef = objRef[key];
+      if (typeof objRef !== 'object') {
+        break;
+      }
+    }
+  })
+
+  return copied
+}
+
 export function includeOrExcludeObjectSync(
   ocv: any,
   paths: string[],
@@ -41,8 +99,8 @@ export function includeOrExcludeObjectSync(
       ? ocv
       : notIncludedSymbol
     : isIncluded // exclude mode, path is in list
-    ? notIncludedSymbol
-    : ocv;
+      ? notIncludedSymbol
+      : ocv;
 }
 
 
@@ -67,6 +125,33 @@ export function objectContainedLoggedSync(
     return JSON.stringify(ocv);
   } else {
     return `${ocv}`;
+  }
+}
+
+export function imObjectContainedLogSync(
+  ocv: any,
+  options?: {
+    include?: string[];
+    exclude?: string[];
+  }
+): string {
+  if (options && typeof ocv === 'object') {
+    if (options.include && options.include.length > 0) {
+      return JSON.stringify(
+        includeObjectSync(ocv, { paths: options.include })
+      );
+    }
+    if (options.exclude && options.exclude.length > 0) {
+      return JSON.stringify(
+        excludeObjectSync(ocv, { paths: options.exclude })
+      )
+    }
+  }
+
+  if (typeof ocv === "object") {
+    return JSON.stringify(ocv);
+  } else {
+    return `${ocv}`
   }
 }
 
