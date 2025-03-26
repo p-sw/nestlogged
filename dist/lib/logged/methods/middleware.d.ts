c7f52ce58f0509246ac37b8418c83c09001fe7ae
Shinwoo PARK <shinwoo.park@psw.kr>
2025-03-26 23:33:01 +0900

    chore: build

--- dist/lib/logged/methods/middleware.d.ts
+++ dist/lib/logged/methods/middleware.d.ts
@@ -1,3 +1,2 @@
 import { OverrideBuildOptions } from '../utils';
-import { ExecutionContext } from '@nestjs/common';
-export declare function LoggedMiddleware<F extends Array<any>, R>(options?: Partial<OverrideBuildOptions>): (_target: any, key: string, descriptor: TypedPropertyDescriptor<(context: ExecutionContext, ...args: F) => R>) => void;
+export declare function LoggedMiddleware<F extends Array<any>, R>(options?: Partial<OverrideBuildOptions>): (_target: any, key: string, descriptor: TypedPropertyDescriptor<(...args: F) => R>) => void;
