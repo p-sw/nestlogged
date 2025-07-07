81ea2c114d9f365181ad7bda1af472d368fb7dd5
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-08 01:06:11 +0900

    feat: add logOptions in LoggedInjectable and LoggedController

--- packages/nestlogged/src/logged/class.ts
+++ packages/nestlogged/src/logged/class.ts
@@ -8,14 +8,22 @@ import { RevRequestMethod } from './utils';
 import { LoggedRouteBuild, LoggedFunctionBuild } from './methods';
 import { logger } from '../internals/object-util';
 import { overrideBuild } from './override';
+import { OverrideBuildOptions } from './utils';
 
 /**
  * @internal
  */
 export function LoggedInjectable(oB: typeof overrideBuild = overrideBuild) {
   return function LoggedInjectable(
-    options?: ScopeOptions & { verbose?: boolean },
+    options?: ScopeOptions & {
+      /** @deprecated use logOptions.verbose instead */
+      verbose?: boolean;
+      logOptions?: Partial<OverrideBuildOptions> & { verbose?: boolean };
+    },
   ) {
+    const logOptions = options?.logOptions ?? {};
+    const verbose = options?.verbose ?? logOptions.verbose ?? false;
+
     return (target: any) => {
       const methods = Object.getOwnPropertyNames(target.prototype);
 
@@ -24,9 +32,9 @@ export function LoggedInjectable(oB: typeof overrideBuild = overrideBuild) {
           method !== 'constructor' &&
           typeof target.prototype[method] === 'function'
         ) {
-          if (options && options.verbose)
+          if (verbose)
             logger.log(`LoggedFunction applied to ${target.name}.${method}`);
-          LoggedFunctionBuild(oB)()(target.prototype, method, {
+          LoggedFunctionBuild(oB)(logOptions)(target.prototype, method, {
             value: target.prototype[method],
           });
         }
@@ -41,19 +49,29 @@ export function LoggedInjectable(oB: typeof overrideBuild = overrideBuild) {
  * @internal
  */
 export function LoggedController(oB: typeof overrideBuild = overrideBuild) {
-  function LoggedController(): (target: any) => void;
-  function LoggedController(prefix: string | string[]): (target: any) => void;
   function LoggedController(
-    options: ControllerOptions & { verbose?: boolean },
+    logOptions?: Partial<OverrideBuildOptions> & { verbose?: boolean },
   ): (target: any) => void;
-  function LoggedController(param?: any): (target: any) => void {
+  function LoggedController(
+    prefix: string | string[],
+    logOptions?: Partial<OverrideBuildOptions> & { verbose?: boolean },
+  ): (target: any) => void;
+  function LoggedController(
+    options: ControllerOptions & {
+      /** @deprecated use logOptions.verbose instead */
+      verbose?: boolean;
+      logOptions?: Partial<OverrideBuildOptions> & { verbose?: boolean };
+    },
+  ): (target: any) => void;
+  function LoggedController(
+    param?: any,
+    _logOptions?: Partial<OverrideBuildOptions> & { verbose?: boolean },
+  ): (target: any) => void {
+    const logOptions = param?.logOptions ?? _logOptions ?? {};
+    const verbose = param?.verbose ?? logOptions.verbose ?? false;
+
     return (target: any) => {
       const methods = Object.getOwnPropertyNames(target.prototype);
-
-      let verbose =
-        typeof param === 'object' && Object.keys(param).includes('verbose')
-          ? param.verbose
-          : false;
 
       methods.forEach((method) => {
         if (
@@ -70,7 +88,7 @@ export function LoggedController(oB: typeof overrideBuild = overrideBuild) {
               `LoggedRoute applied to ${target.name}.${method} (${RevRequestMethod[httpMethod]} ${path})`,
             );
           }
-          LoggedRouteBuild(oB)()(target.prototype, method, {
+          LoggedRouteBuild(oB)(logOptions)(target.prototype, method, {
             value: target.prototype[method],
           });
         }
